18825c3e8ecdab96b82a285471ae21b5af62d84b
ws-coredev <ledinhbinhit@gmail.com>
2025-01-08 10:44:00 +0700

    Install packages

---
+++ Presentation/App/IFIN.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Presentation.App
+{
+    public class IFIN
+    {
+        public static IServiceProvider ServiceProvider { get; private set; }
+
+        public static void Bootstrap()
+        {
+            _SetupSQLite();
+        }
+
+        private static void _SetupSQLite()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # IFIN
+
+## Install nuget packages via Package Manager Console
+ 
+- To restore all packages of all projects:
+ ```
+ Update-Package -Reinstall
+ ```
+- To install a particular package to a specific project:
+ ```
+ Install-Package Dapper -Version 2.1.35 -Project Presentation
+ ```
